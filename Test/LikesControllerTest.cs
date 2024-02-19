0beeccf5b59e0894d0e61680779fd38ede712c63
Nick <mtaghavinima@gmail.com>
2024-02-18 23:44:32 -0500

    Added test "AddLike_InvalidUsername_ReturnsNotFoundResult"

--- Test/LikesControllerTest.cs
+++ Test/LikesControllerTest.cs
@@ -16,6 +16,43 @@ namespace Test;
 public class LikesControllerTest
 {
     [Fact]
+    public async Task AddLike_InvalidUsername_ReturnsNotFoundResult()
+    {
+        // Arrange
+        var username = "nonexistentUser";
+        var sourceUserId = 10;
+
+        var userRepositoryMock = new Mock<IUserRepository>();
+        userRepositoryMock.Setup(repo => repo.GetUserByUsernameAsync(username))
+            .ReturnsAsync((AppUser)null);
+
+        var likesRepositoryMock = new Mock<ILikesRepository>();
+        // setup for GetUserWithLikes and GetUserLike can be reused from the previous test
+    
+        var unitOfWorkMock = new Mock<IUnitOfWork>();
+        unitOfWorkMock.SetupGet(uow => uow.UserRepository).Returns(userRepositoryMock.Object);
+        unitOfWorkMock.SetupGet(uow => uow.LikesRepository).Returns(likesRepositoryMock.Object);
+
+        var user = new ClaimsPrincipal(new ClaimsIdentity(new Claim[]
+        {
+            new Claim(ClaimTypes.Name, "example name"),
+            new Claim(ClaimTypes.NameIdentifier, sourceUserId.ToString()),
+            new Claim("custom-claim", "example claim value"),
+        }, "mock"));
+
+        var controller = new LikesController(unitOfWorkMock.Object);
+
+        controller.ControllerContext.HttpContext = new DefaultHttpContext() { User = user };
+
+        // Act
+        var result = await controller.AddLike(username);
+
+        // Assert
+        var notFoundResult = Assert.IsType<NotFoundResult>(result);
+        Assert.NotNull(notFoundResult);
+    }
+
+    [Fact]
     public async Task AddLike_ValidUsername_ReturnsOkResult()
     {
         // Arrange
